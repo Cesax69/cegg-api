4eeeacbef14c9ab217d6f0dc98d9ba338d989262
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-03-03 19:00:53 -0600

    feat: instalar Prisma CLI, inicializar esquema (SQLite), agregar ConfigModule y modelos User/Post

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthService } from './modules/auth/auth.service';
 import { TaskModule } from './modules/task/task.module';
+import { PrismaService } from './prisma.service';
+import { UsersService } from './user.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, TaskModule],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, UsersService],
 })
 export class AppModule { }
